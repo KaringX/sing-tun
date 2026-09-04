b9a9d435493c8d12032b1ddf93150bf74a4cf981
GooRingX <gooringx@gmail.com>
2026-09-04 16:12:39 +0800

    update

--- tun_darwin.go
+++ tun_darwin.go
@@ -10,12 +10,9 @@ import (
 	"syscall"
 	"unsafe"
 
-	"github.com/sagernet/sing-tun/internal/gtcpip/header"
-	rawfile "github.com/sagernet/sing-tun/internal/rawfile_darwin"
-	stopfd "github.com/sagernet/sing-tun/internal/stopfd_darwin"
-
 	"github.com/sagernet/sing-tun/gtcpip/header"
 	rawfile "github.com/sagernet/sing-tun/internal/rawfile_darwin"
+	stopfd "github.com/sagernet/sing-tun/internal/stopfd_darwin"
 
 	"github.com/sagernet/sing/common"
 	"github.com/sagernet/sing/common/buf"
